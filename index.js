0c5a915614319bd49270c600ab76fbca8a380ea8
Alexander Spanos <alexisspa9@gmail.com>
2018-03-18 19:38:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var katzDeliLine = [];
+
+function takeANumber(line, name) {
+    var position = line.length + 1;
+    line.push(name);
+    console.log(`Welcome, ${name}. You are number ${position} in line.`);
+}
+
+function nowServing() {
+  if (katzDeliLine.length > 0) {
+  return katzDeliLine[0];
+  }
+  else {
+  console.log('There is nobody waiting to be served!');
+  }
+}
+
+function currentLine() {
+    if (katzDeliLine.length > 0) {
+  var i = 0;
+  var position = i + 1
+  var text = "The line is currently: "
+  while (katzDeliLine.length >= 1) {
+    text.push(`${position}. ${katzDeliLine[i]},`);
+  }
+  console.log(text);
+    }
+    else {
+      console.log('The line is currently empty.')
+    }
+}
